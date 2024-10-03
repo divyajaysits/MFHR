6c9d1de6e4ab6af76bbcafacac9c63cdd56b501f
divya.j <divya.j@DIVYA-SSD-LT>
2024-10-03 17:56:08 +0530

    finalized migration script

---
+++ MFHR.Server/Migrations/20241003122532_Added DeptId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MF.HR.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDeptId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentId",
+                table: "MFHR_Employees",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "MFHR_Employees");
+        }
+    }
+}
